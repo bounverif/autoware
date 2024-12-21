eb1c59fe3e2ba4c6a6975bb6a44d2f47d99f4349
Dogan Ulus <dogan.ulus@bogazici.edu.tr>
2024-12-21 16:17:40 +0300

    Add build script and fix caching process

---
+++ containers/autoware-devel/build-autoware.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+git clone "${AUTOWARE_SOURCE_REPO}" --depth=1 "${AUTOWARE_DEVEL_ROOT}"
+mkdir -p "${AUTOWARE_DEVEL_SOURCE_DIR}"
+vcs import "${AUTOWARE_DEVEL_SOURCE_DIR}" --shallow --input "${AUTOWARE_DEVEL_ROOT}/autoware.repos"
+. /opt/ros/humble/setup.sh 
+ccache --show-config 
+ccache --zero-stats
+colcon --log-base /dev/null build \
+    --packages-up-to "${AUTOWARE_BUILD_PACKAGES_UP_TO}" \
+    --base-paths "${AUTOWARE_DEVEL_SOURCE_DIR}" \
+    --build-base "${AUTOWARE_DEVEL_BUILD_DIR}" \
+    --install-base "${AUTOWARE_DEVEL_INSTALL_DIR}" \
+    --parallel-workers "${AUTOWARE_BUILD_PARALLEL_WORKERS:-4}" \
+    --merge-install \
+    --event-handlers \
+        console_direct- \
+        console_stderr+ \
+        console_cohesion- \
+        console_start_end- \
+        console_package_list+ \
+        status- \
+        summary+ \
+        desktop_notification-
+ccache -v --show-stats
